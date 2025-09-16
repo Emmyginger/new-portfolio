b0cf4fff0f67d75622f90d0a1bf4220a73f48046
Israel Orizu <israelorizu@gmail.com>
2025-09-17 00:43:54 +0100

    added footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Israel Orizu. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
